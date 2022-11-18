3fb3f49fcfaff20bc6f0e3e0841730096a548e76
islomjon6975 <islomjon0919@mail.ru>
2022-11-18 19:29:33 +0500

    image

---
+++ assets/script.pdp.js
@@ -0,0 +1,8 @@
+
+// product image
+const image = document.querySelector('.pdp__image--wrapper');
+const imageModal = document.querySelector('.image-modal');
+
+image.addEventListener('click', () => {
+    imageModal.style.display = 'flex'
+})
